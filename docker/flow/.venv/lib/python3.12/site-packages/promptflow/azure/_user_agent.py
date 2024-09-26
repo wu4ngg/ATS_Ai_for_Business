31f66b1be3d120bd81939d934251010f8071d960
fowardslash <quangrain2014@gmail.com>
2024-09-26 13:33:36 +0700

    remove docker directory

--- docker/flow/.venv/lib/python3.12/site-packages/promptflow/azure/_user_agent.py
+++
@@ -1,7 +0,0 @@
-# ---------------------------------------------------------
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# ---------------------------------------------------------
-
-from promptflow.azure._version import VERSION
-
-USER_AGENT = "{}/{}".format("promptflow-azure-sdk", VERSION)
--- docker/flow/.venv/lib/python3.12/site-packages/promptflow/tools/__init__.py
+++
@@ -1,3 +0,0 @@
-from .aoai import AzureOpenAI  # noqa: F401
-from .openai import OpenAI  # noqa: F401
-from .serpapi import SerpAPI  # noqa: F401
